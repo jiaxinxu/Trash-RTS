3628f1edbccadd3d2aafa37fa04d3f09dbe2e0c9
Elliott Mahler <join.together@gmail.com>
2016-09-27 13:53:55 -0700

    Updated character selection logic

--- Assets/Scripts/selectObject.cs
+++ Assets/Scripts/selectObject.cs
@@ -1,33 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class selectObject : MonoBehaviour {
-	private GameObject selectedUnit;
+public class selectObject : MonoBehaviour
+{
+    private GameObject selectedUnit;
 
-	void  Start (){
-		
-	}
+    void Start()
+    {
 
-	void  Update (){
-		RaycastHit hit;
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if (Physics.Raycast(ray, out hit))
-		{
-			if (hit.transform.tag == "Trash" && Input.GetMouseButtonDown(0))
-			{
-				selectedUnit = hit.transform.gameObject;
-				selectedUnit.SendMessage("Select", 1);
-			}
-			if (hit.transform.tag == "Table" && Input.GetMouseButtonDown(1))
-			{
-				selectedUnit.SendMessage("Destination", hit.point);
-			}
-			if (hit.transform.tag == "Table" && Input.GetMouseButtonDown(0))
-			{
-				selectedUnit.SendMessage("Deselect", 1);
-			}
-		}
-	}
+    }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out hit))
+            {
+                //if the user left clicked, select or deselect
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (hit.transform.tag == "Trash")
+                    {
+                        selectedUnit = hit.transform.gameObject;
+                        selectedUnit.SendMessage("Select", 1);
+                    }
+                    else
+                    {
+                        if (selectedUnit != null)
+                        {
+                            selectedUnit.SendMessage("Deselect", 1);
+                        }
+                        selectedUnit = null;
+                    }
+                }
+
+                //if the player right clicked, tell the selected unit to move
+                else if (Input.GetMouseButtonDown(1))
+                {
+                    if (selectedUnit != null)
+                    {
+                        selectedUnit.SendMessage("Destination", hit.point);
+                    }
+                }
+            }
+        }
+    }
 }
