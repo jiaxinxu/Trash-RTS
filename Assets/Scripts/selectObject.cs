c6844178e1cdd0a035e58cba71d32eb6e0403f7a
Jiaxin <nobodyyago@hotmail.com>
2016-09-26 19:15:34 -0700

    Game Scene
    Build a game scene

---
+++ Assets/Scripts/selectObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectObject : MonoBehaviour {
+	public GameObject selectedUnit;
+
+	void  Start (){
+		
+	}
+
+	void  Update (){
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit))
+		{
+			if (hit.transform.tag == "Player" && Input.GetMouseButtonDown(0))
+			{
+				selectedUnit = hit.transform.gameObject;
+				selectedUnit.SendMessage("Select", 1);
+			}
+			if (hit.transform.tag == "Table" && Input.GetMouseButtonDown(1))
+			{
+				selectedUnit.SendMessage("Destination", hit.point);
+			}
+			if (hit.transform.tag == "Table" && Input.GetMouseButtonDown(0))
+			{
+				selectedUnit.SendMessage("Deselect", 1);
+			}
+		}
+	}
+
+
+}
